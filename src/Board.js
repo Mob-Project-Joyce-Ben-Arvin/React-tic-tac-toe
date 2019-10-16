4c69d6b8265cb0e507f2ce65f80b6b6ff5a8b98f
Ubuntu <ubuntu@ip-172-31-47-73.us-east-2.compute.internal>
2019-10-16 21:55:27 +0000

    Setup Base for Game

--- src/Board.js
+++ src/Board.js
@@ -1,15 +1,24 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Tile from './Tile.js'
 
 function Board() {
     const [player, setPlayer] = useState(true)
-    const switchPlayer = _ => setPlayer(!player)
-  return (
-    <div className="App">
-        <h2>{player ? "Player One" : "Player Two"}</h2>
-        <Tile handleClick={switchPlayer}/>
-    </div>
-  );
+    const board = Array(9).fill(null)
+    const switchPlayer = index => {
+        console.log("Switch player after ",index," was clicked")
+        setPlayer(!player)
+    }
+    const generateTile = (elm, ind) => (
+        <Tile handleClick={switchPlayer} player={player} index={ind} key={ind}/>
+    )
+    
+    useEffect(()=> console.log("Player ::",player))
+    return (
+        <div className="App">
+            <h2>{player ? "Player One" : "Player Two"}</h2>
+            {board.map(generateTile)}
+        </div>
+    );
 }
 
 export default Board;
--- src/Tile.js
+++ src/Tile.js
@@ -1,17 +1,21 @@
 import React, {useState} from 'react'
 
 function Tile(props) {
-    const [player, setPlayer] = useState(true)
+    const [tile, setTile] = useState(" Empty ")
+    const [beenClicked, setClick] = useState(false)
     const onClick = _ => {
-        props.handleClick()
-        setPlayer(!player)
+        if(!beenClicked){
+            props.handleClick(props.index)
+            setTile(props.player ? 'X' : 'O')
+            setClick(true)
+        }
     }
-  return (
-    <div className="App">
-        <h2>{player ? "X" : "O"}</h2>
-        <button onClick={onClick}> Switch Player </button>
-    </div>
-  );
+    
+    return (
+        <div className="App">
+            <h2 onClick={onClick}>{tile}</h2>
+        </div>
+    );
 }
 
 export default Tile
