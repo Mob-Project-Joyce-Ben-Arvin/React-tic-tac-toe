098aafb22ecb7c3fb6a96c4a951487356fbc26c5
Ubuntu <ubuntu@ip-172-31-47-73.us-east-2.compute.internal>
2019-10-17 22:59:09 +0000

    added scoredboard

--- src/Board.js
+++ src/Board.js
@@ -12,18 +12,22 @@ function Board() {
     
     //Constants
     const board = Array(9).fill(null)
-    const winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[3,5,8],[0,4,8],[2,4,6]]
+    const winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     
     //Game Logic
     const hasWon = (playerTurn, index) => !!winConditions.filter(wincondition => wincondition.every(winNum => [...playerTurn, index].includes(winNum))).length
     
     const gameLogic = index => {
         const winnerFound = hasWon(player ? p1Moves : p2Moves, index)
+        
         player ? setP1([...p1Moves,index]) : setP2([...p2Moves,index])
 
         if(winnerFound){
             setWinner(winnerFound)
+            
+            player ? setScores([scores[0] +1 , scores[1]]) : setScores([scores[0], scores[1] + 1])
             //Stop game
+            
         }else{
             setPlayer(!player)
         }
@@ -41,12 +45,14 @@ function Board() {
             <div className="game_container">
                 <div className="score_container">
                     <h1>Player 1</h1>
+                        <h3>{scores[0]}</h3>
                 </div>
                 <div className="grid">
                     {board.map(generateTile)}
                 </div>
                 <div className="score_container">
-                    <h1>Player 2</h1>
+                    <h1>Player 2 </h1>
+                    <h3>{scores[1]}</h3>
                 </div>
             </div>
         </>
